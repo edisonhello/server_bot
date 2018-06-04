f66de50c0dc05fe846bfd1389e59f83898bff81b
edisonhello <edisonhello@hotmail.com>
2018-06-04 16:45:05 +0800

    able to use command line

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const config = require('./config.json')
 const request = require('request')
 const cheerio = require('cheerio')
 
+const childProcess = require('child_process')
+
 const tgbotapi = require('node-telegram-bot-api')
 
 const bot = new tgbotapi(config.token, {
@@ -19,9 +21,23 @@ var intervalId = setInterval(() => {
 bot.sendMessage(config.self_tgID, 'Server restarted.')
 
 bot.on('message', msg => {
-    if( msg.from.id != config.self_tgID ) return
-    if( msg.text === 'check' ) {
+    // console.log( msg )
+    if( msg.from.id != config.self_tgID ) {
+        bot.sendMessage(msg.from.id, 'You have no permission to talk with me.')
+        return
+    }
+    args = msg.text.split(' ')
+    console.log(args)
+    if( args[0] === 'check' ) {
         bot.sendMessage(msg.from.id, 'Server is running now.')
+    }
+    if( args[0] === '$' ) {
+        args.splice(0, 1)
+        let command = args.join(' ')
+        let res = childProcess.execSync(command, {
+            encoding: 'ascii'
+        })
+        bot.sendMessage(msg.from.id, res)
     }
     else bot.sendMessage(msg.from.id, 'What?')
 })
