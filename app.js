4eb82e3d2e4c63cc768ac9cde48dda0333c0b68a
edisonhello <edisonhello@hotmail.com>
2018-06-24 22:12:50 +0800

    add regist and getip command
    allow registed user to get server ip

--- app.js
+++ app.js
@@ -20,37 +20,60 @@ var intervalId = setInterval(() => {
 
 bot.sendMessage(config.self_tgID, 'Server restarted.')
 
+function noPermission(sender) {
+    console.log('No permission.')
+    bot.sendMessage(sender, 'You have no permission to use this.')
+}
+
 bot.on('message', msg => {
     // console.log( msg )
     let sender = msg.from.id
-    if( sender != config.self_tgID ) return bot.sendMessage(sender, 'You have no permission to talk with me.')
+    // if( sender != config.self_tgID ) return bot.sendMessage(sender, 'You have no permission to talk with me.')
     args = msg.text.split(' ')
     console.log(args)
     if( args[0] === 'check' ) {
         bot.sendMessage(sender, 'Server is running now.')
     }
     else if( args[0] === 'minecraft' ) {
-        let res = childProcess.execSync('tmux ls', { encoding:'ascii' })
-        let serverRunning = (res.indexOf('minecraft') !== -1)
-        if( args[1] === undefined ) {
-            bot.sendMessage(sender, serverRunning ?
-                'Minecraft server is running.' :
-                'Minecraft server is not running.')
-        }
-        else if( args[1] === 'start' ) {
-            if( serverRunning ) return bot.sendMessage(sender, 'Minecraft server is already running.')
-            childProcess.execSync('tmux new-session -d -s minecraft \'~/minecraft/IE/ServerStart.sh\'')
-        }
-        else if( args[1] === 'ssttoopp' ) {
-            if( !serverRunning ) return bot.sendMessage(sender, 'Minecraft server is not running now.')
-            childProcess.execSync('tmux kill-session -s minecraft')
-        }
+        if(sender != config.self_tgID) return noPermission(sender)
+        childProcess.exec('tmux ls', { encoding:'ascii' }, (err, res, stderr) => {
+            let serverRunning = (res.indexOf('minecraft') !== -1)
+            if( args[1] === undefined ) {
+                bot.sendMessage(sender, serverRunning ?
+                    'Minecraft server is running.' :
+                    'Minecraft server is not running.')
+            }
+            else if( args[1] === 'start' ) {
+                if( serverRunning ) return bot.sendMessage(sender, 'Minecraft server is already running.')
+                childProcess.execSync(`tmux new-session -d -s minecraft '~/minecraft/IE/ServerStart.sh'`)
+                bot.sendMessage(sender, 'Maybe server is opened.')
+            }
+            else if( args[1] === 'ssttoopp' ) {
+                if( !serverRunning ) return bot.sendMessage(sender, 'Minecraft server is not running now.')
+                childProcess.execSync('tmux kill-session -s minecraft')
+                bot.sendMessage(sender, 'Maybe server is closed.')
+            }
+        })
     }
     else if( args[0] === '$' ) {
+        if(sender != config.self_tgID) return noPermission(sender)
         args.splice(0, 1)
         let command = args.join(' ')
         let res = childProcess.execSync(command, {  encoding:'ascii' })
         bot.sendMessage(sender, res)
+    }
+    else if( args[0] === 'getip' ) {
+        console.log(config.registedUser[args[0]], sender)
+        if( !config.registedUser[args[0]].includes(sender) ) return noPermission(sender)
+        request.get({ url: 'https://myip.com.tw/' }, (err, header, body) => {
+            if(err) return bot.sendMessage(sender, 'Error happen when getting ip.' + err)
+            let $ = cheerio.load(body, { decodeEntities: false })
+            bot.sendMessage(sender, 'Server\'s ip is ' + $('h1 font').html())
+        })
+    }
+    else if( args[0] === 'regist') {
+        bot.sendMessage(config.self_tgID, 'Receive regist request: \n' + JSON.stringify(msg, null, 4))
+        bot.sendMessage(sender, 'Received regist request, please wait for apply.')
     }
     else bot.sendMessage(sender, 'What?')
 })
